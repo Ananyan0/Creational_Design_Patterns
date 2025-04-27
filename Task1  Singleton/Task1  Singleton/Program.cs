c6c1f4167e84dfbc32a065964900fab938ce95bb
Karen-xgit <ananyan2020>
2025-04-28 02:56:39 +0400

    add Project Task<1> Singleton

---
+++ Task1  Singleton/Task1  Singleton/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Singleton;
+
+class ConfigurationManager
+{
+	public string Theme { get; set;}
+	public string Language { get; set; }
+	public double Version { get; set; }
+	private static ConfigurationManager _instance;
+
+	private ConfigurationManager() { }
+
+
+	public static ConfigurationManager GetInstance()
+	{
+		if(_instance == null)
+		{
+			_instance = new ConfigurationManager();
+		}
+		return _instance;
+	}
+}
+
+class Program 
+{
+    public static void Main(string[] args)
+    {
+		ConfigurationManager instance = ConfigurationManager.GetInstance();
+
+	}
+}
